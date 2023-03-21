92151d1121be4ba850a2488d6d17e096fe1a9799
kumar.abhiii <abhiiishekparmar@gmail.com>
2023-03-21 21:53:38 +0530

    Initial Commit

---
+++ src/main/java/com/kumar/abhiii/ToDoList/Main.java
@@ -0,0 +1,10 @@
+package com.kumar.abhiii.ToDoList;
+public class Main
+{
+public static void main(String[] args) {
+    ToDoList toDoList = new ToDoList();
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        toDoList.save();
+    }));
+}
+}
