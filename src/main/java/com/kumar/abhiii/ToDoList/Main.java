21324ad7f8a697738ff84d6f572621026d2d33bd
kumar.abhiii <abhiiishekparmar@gmail.com>
2023-03-21 21:55:12 +0530

    update main

--- src/main/java/com/kumar/abhiii/ToDoList/Main.java
+++ src/main/java/com/kumar/abhiii/ToDoList/Main.java
@@ -2,9 +2,9 @@ package com.kumar.abhiii.ToDoList;
 public class Main
 {
 public static void main(String[] args) {
-    ToDoList toDoList = new ToDoList();
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        toDoList.save();
-    }));
+ToDoList toDoList = new ToDoList();
+Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+toDoList.save();
+}));
 }
 }
